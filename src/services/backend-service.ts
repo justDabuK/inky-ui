fe120e7ea0944b5bf66cbaf0763cab570f785748
justDaBuK <kevinjust87@gmail.com>
2020-12-28 21:37:48 +0100

    back to full name

--- src/services/backend-service.ts
+++ src/services/backend-service.ts
@@ -1,6 +1,6 @@
 import { DefaultApi } from "@/services/backend/generated/api/default-api";
 
-const BASE_PATH = "http://localhost:8000";
+const BASE_PATH = "http://inky.fritz.box:8000";
 
 export const API = new DefaultApi({
   basePath: BASE_PATH
