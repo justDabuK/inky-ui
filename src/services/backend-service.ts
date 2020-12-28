68246a7df9579abf28c794c55d23cbec4965ecec
justDaBuK <kevinjust87@gmail.com>
2020-12-28 21:38:04 +0100

    tried ip

--- src/services/backend-service.ts
+++ src/services/backend-service.ts
@@ -1,6 +1,6 @@
 import { DefaultApi } from "@/services/backend/generated/api/default-api";
 
-const BASE_PATH = "http://inky.fritz.box:8000";
+const BASE_PATH = "http://192.168.178.77:8000";
 
 export const API = new DefaultApi({
   basePath: BASE_PATH
