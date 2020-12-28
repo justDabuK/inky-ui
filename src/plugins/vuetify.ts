1eeed4b0f4fdfe2e48d84eb9256bc03ffcc00241
justDaBuK <kevinjust87@gmail.com>
2020-12-28 17:15:56 +0100

    basic app layout

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,6 +1,18 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
+import { colors } from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.blueGrey.base,
+        secondary: colors.cyan.base,
+        accent: "#8c9eff",
+        error: "#b71c1c"
+      }
+    }
+  }
+});
