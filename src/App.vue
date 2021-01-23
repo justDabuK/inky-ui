52c8e60023af28a1e0e0017e4a232bb4ed4039c2
justDaBuK <kevinjust87@gmail.com>
2021-01-23 16:34:39 +0100

    refactor - introduced MainView

--- src/App.vue
+++ src/App.vue
@@ -1,131 +1,14 @@
 <template>
   <v-app>
-    <v-card class="pb-2">
-      <v-app-bar color="primary" dark>
-        <v-app-bar-title>inky UI</v-app-bar-title>
-        <template #extension>
-          <v-tabs align-with-title v-model="tab">
-            <v-tab key="select">select</v-tab>
-            <v-tab key="upload">upload</v-tab>
-          </v-tabs>
-        </template>
-      </v-app-bar>
-      <v-tabs-items v-model="tab">
-        <v-tab-item key="select">
-          <selection-panel
-            :available-images="availableImages"
-            :headers="headers"
-            :setting="setting"
-            :loading="loading"
-            @set-selected-image="setSelectedImage"
-            @update-images="updateImages"
-          />
-        </v-tab-item>
-        <v-tab-item key="upload">
-          <upload-panel
-            :original-images="originalImages"
-            :uploading="uploading"
-            :cropping="cropping"
-            @upload-and-crop="uploadAndCrop"
-          />
-        </v-tab-item>
-      </v-tabs-items>
-    </v-card>
+    <main-view />
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { API } from "@/services/backend-service";
-import _ from "lodash";
-import SelectionPanel from "@/components/SelectionPanel.vue";
-import UploadPanel from "@/components/UploadPanel.vue";
+import MainView from "@/components/MainView.vue";
 
 export default Vue.extend({
-  name: "App",
-
-  components: { UploadPanel, SelectionPanel },
-
-  data() {
-    return {
-      tab: null,
-      loading: false,
-      setting: false,
-      uploading: false,
-      cropping: false,
-      adjustedImages: [] as string[],
-      originalImages: [] as string[],
-      headers: [
-        {
-          text: "Name",
-          align: "start",
-          sortable: true,
-          value: "name"
-        }
-      ]
-    };
-  },
-  async created() {
-    await this.updateImages();
-  },
-  computed: {
-    availableImages(): { id: string; name: string }[] {
-      return this.adjustedImages.map(img => {
-        return {
-          id: img,
-          name: _.upperFirst(_.lowerCase(img.split(".")[0]))
-        };
-      });
-    }
-  },
-  methods: {
-    async getAdjustedImages(): Promise<void> {
-      this.loading = true;
-      const response = (await API.getImagesImagesAdjustedGet()) as {
-        data: string[];
-      };
-      this.adjustedImages = response.data;
-      this.loading = false;
-    },
-
-    async getOriginalImages(): Promise<void> {
-      this.loading = true;
-      const response = (await API.getImagesImagesOriginalGet()) as {
-        data: string[];
-      };
-      this.originalImages = response.data;
-      this.loading = false;
-    },
-
-    async updateImages(): Promise<void> {
-      await this.getAdjustedImages();
-      await this.getOriginalImages();
-    },
-
-    async setSelectedImage(imageName: string): Promise<void> {
-      this.setting = true;
-      await API.setImageImagesSetImageNamePut(imageName);
-      this.setting = false;
-    },
-
-    async uploadAndCrop(sourceFile: File | undefined): Promise<void> {
-      if (sourceFile) {
-        try {
-          this.uploading = true;
-          await API.uploadFileUploadfilePost(sourceFile);
-          this.uploading = false;
-          this.cropping = true;
-          await API.cropImageForInkyImagesCropImageNamePut(sourceFile.name);
-          this.cropping = false;
-        } catch (exception) {
-          throw new Error(exception);
-        } finally {
-          this.uploading = false;
-          this.cropping = false;
-        }
-      }
-      await this.updateImages();
-    }
-  }
+  components: { MainView }
 });
 </script>
