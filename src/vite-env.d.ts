77a706657d28b18c353078069f267e3d1e04718a
justDabuK <kevinjust87@gmail.com>
2023-04-11 19:02:39 +0200

    fix: needed to declare module for the build

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,3 +6,4 @@ declare module 'vue-material-design-icons/ChevronLeft.vue';
 declare module 'vue-material-design-icons/ChevronRight.vue';
 declare module 'vue-material-design-icons/ProgressUpload.vue';
 declare module 'vue-material-design-icons/Eye.vue';
+declare module 'vue-material-design-icons/Loading.vue';
