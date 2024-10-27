9cd43b0382fb5cedde28abcf056489aaa31c6c19
justDaBuK <kevinjust87@gmail.com>
2024-10-27 22:59:05 +0100

    fix: update ip adress

--- src/services/backend-service.ts
+++ src/services/backend-service.ts
@@ -1,6 +1,6 @@
 import { Configuration, DefaultApi } from './backend/generated';
 
-const BASE_PATH = 'http://192.168.178.30:8000';
+const BASE_PATH = 'http://192.168.178.29:8000';
 
 export const API = new DefaultApi(
   new Configuration({
